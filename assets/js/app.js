02088ef775d056253869331f5294398dbc2ad361
unknown <beisongabc@gmail.com>
2019-11-03 12:09:54 +0100

    timer in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
     if ($(".preloader-wrap").length) {
         $('.preloader-wrap').delay(1000).fadeOut('slow');
     }
+    
+    
+    var clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter'
+    });
 
 
 });

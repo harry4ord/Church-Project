5f74586898addbf9b9c492ed3ce60b0080615c3f
Benjamin <beisongabc@gmail.com>
2019-11-06 19:18:35 +0100

    countdown timer fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,39 @@
+var clock;
 $(document).ready(function () {
     if ($(".preloader-wrap").length) {
         $('.preloader-wrap').delay(1000).fadeOut('slow');
     }
     
-    
-    var clock = $('.clock').FlipClock({
-        clockFace: 'HourlyCounter'
+    var currentDate = new Date();
+        
+    var targetDate = new Date('2019-11-10');
+    var diff = targetDate.getTime() / 1000 - currentDate.getTime() / 1000;
+    clock = $('.clock').FlipClock( diff, {
+        clockFace: 'HourlyCounter',
+        countdown: true
     });
 
 
 });
+
+
+//			var clock;
+//
+//			$(document).ready(function() {
+//
+//				// Grab the current date
+//				var currentDate = new Date();
+//
+//				// Set some date in the future. In this case, it's always Jan 1
+//				var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+//
+//				// Calculate the difference in seconds between the future and current date
+//				var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+//
+//				// Instantiate a coutdown FlipClock
+//				clock = $('.clock').FlipClock(diff, {
+//					clockFace: 'DailyCounter',
+//					countdown: true
+//				});
+//			});
+		
